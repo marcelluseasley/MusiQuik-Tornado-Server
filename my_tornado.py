4955d77e66929f4cdb1026e2f02947701a862c45
Marcellus Easley <marcellus.easley@gmail.com>
2015-11-16 04:39:10 +0000

    example content

--- my_tornado.py
+++ my_tornado.py
@@ -1,1 +1,28 @@
 __author__ = 'measley'
+
+import os.path
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+
+define("port", default=8080, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write("hello, world")
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r'/', IndexHandler)
+        ],
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
